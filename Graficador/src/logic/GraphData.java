07b8363b9d737f1382fb74bdbb6b272543546296
José Andrés García <jags9415@gmail.com>
2014-04-17 20:58:47 -0600

    Se documento la clase.

--- Graficador/src/logic/GraphData.java
+++ Graficador/src/logic/GraphData.java
@@ -1,70 +1,67 @@
 package logic;
 
+import java.util.Arrays;
+
+/**
+ * La clase GraphData implementa un contenedor para la información representada en un gráfico.
+ */
 public class GraphData {
     
     private int[] keys;
     private int[] values;
     
+    /**
+     * Crea un nuevo objeto GraphData.
+     */
     public GraphData() {
         this.keys = new int[0];
         this.values = new int[0];
     }
 
-    public void setRange(int ini, int fin){
+    /**
+     * Establece el rango en el que estarán los valores del gráfico.
+     * 
+     * @param ini Inicio del rango.
+     * @param fin Final del rango.
+     */
+    public void setRange(int ini, int fin) {
         this.keys = new int[fin-ini+1];
         this.values = new int[fin-ini+1];
         
-        int cont = 0;
-        while (ini <= fin) {
-            this.keys[cont] = ini++;
-            this.values[cont++] = 0;
+        for (int i=0; i<=fin-ini; i++) {
+            this.keys[i] = ini+i;
+            this.values[i] = 0;
         }
     }
     
-    public int getCount(int key)
-    {
-        int index = binarySearch(key);
-        if (index != -1) {
-            return this.values[index];
-        }
-        else {
-            return -1;
-        }
-    }
-    
+    /**
+     * Incrementa el peso almacenado para cierto valor.
+     * 
+     * @param key Valor a incrementar.
+     */
     public void incrementCount(int key) {
-        int index = binarySearch(key);
+        int index = Arrays.binarySearch(this.keys, key);
         if (index != -1) {
             this.values[index]++;
         }
     }
     
+    /**
+     * Obtiene el conjunto de valores.
+     * 
+     * @return El conjunto de valores.
+     */
     public int[] getKeys() {
         return this.keys;
     }
     
+    /**
+     * Obtiene el conjunto de pesos de los valores.
+     * 
+     * @return El conjunto de pesos de los valores.
+     */
     public int[] getValues() {
         return this.values;
-    }
-    
-    private int binarySearch(int key) {
-        int hi = this.keys.length - 1;
-        int lo = 0;
-        int index;
-        
-        while (hi >= lo) {
-            index = lo + ((hi - lo) / 2);
-            if (this.keys[index] > key) {
-                hi = index - 1;
-            } 
-            else if (this.keys[index] < key) {
-                lo = index + 1;
-            }
-            else {
-                return index;
-            }
-        }
-        return -1;
     }
     
 }

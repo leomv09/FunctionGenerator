2b8a592050b050a6c86bd9982052ae80827d2637
José Andrés García <jags9415@gmail.com>
2014-04-27 17:36:46 -0600

    Comentar clase.

--- Graficador/src/logic/Utilities.java
+++ Graficador/src/logic/Utilities.java
@@ -1,7 +1,21 @@
 package logic;
 
+/**
+ * Clase que provee ciertas funciones útiles para el sistema.
+ */
 public class Utilities 
 {
+
+    /**
+     * Realiza un split del String que le ingresa y convierte cada valor a double.
+     * Ejemplo: splitAndCast("0:1:0.4", ":") => {0.0, 1.0, 0.4}
+     * 
+     * @param s El String que se desea parsear.
+     * @param delimit El delimitador usado para realizar el split.
+     * @return Un arreglo de double con cada valor del resultado del split.
+     * 
+     * @throws NumberFormatException Si el String no se puede parsear.
+     */
     public static double[] splitAndCast(String s, String delimit) throws NumberFormatException
     {
         String[] temp = s.split(delimit);
